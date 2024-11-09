13bc0e1f477ca726dcc62e75cc30386a16481447
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-11-09 17:34:13 -0600

    Polished buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
-document.querySelector("#submit").addEventListener
+
+document.querySelector('button').addEventListener("click", (event) => {
+    const clickedButton = event.target;
+    clickedButton.setAttribute('hidden', 'true');
+});

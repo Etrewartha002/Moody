f8f407d822929eb7a6dec665cb2ff91fe53bbb75
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-11-09 14:49:37 -0600

    initialized npm

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
-document.querySelector("#submit").addEventListener
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    var passwordInput = document.getElementById('password').value;
+    var usernameInput = document.getElementById('username').value;
+});

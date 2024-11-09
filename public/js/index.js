272a55f6b2f427b4d39ba5978d435aad467a8e85
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-11-09 15:30:59 -0600

    added users

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
+import fs from ("fs");
+function getUsers(){
+    try{
+        const data = fs.readFile
+    }
+}
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault(); 
     var passwordInput = document.getElementById('password').value;

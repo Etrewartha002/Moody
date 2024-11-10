3d52904fdf16f85db735a7160cb5fbf849754335
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-11-09 20:24:10 -0600

    fixed buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
 
-document.querySelector('button').addEventListener("click", (event) => {
+document.querySelector('button').array.forEach(button => {
+    button.addEventListener("click", (event) => {
     const clickedButton = event.target;
     document.querySelector("#part1").setAttribute('hidden', 'true');
-    const buttonContent = clickedButton.innerHTML;
+    const buttonContent = clickedButton.innerHTML.trim();
+    alert(buttonContent);
     switch (buttonContent) {
         case "Happy":
             document.querySelector("body").style.backgroundColor = "rgb(255, 253, 55)";
@@ -15,9 +17,10 @@ document.querySelector('button').addEventListener("click", (event) => {
           break;
         case "Excited":
             document.querySelector("body").style.backgroundColor = "rgb(246, 38, 129)";
+            break;
         case "Angry":
             document.querySelector("body").style.backgroundColor = "rgb(208, 28, 31)";
             break;
         case "Calm":
             document.querySelector("body").style.backgroundColor = "rgb(147, 202, 237)";
-}})
+}})});

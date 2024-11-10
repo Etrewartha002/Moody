9b1a320c119d1f6475acd52f8c71cb404f9cf6e5
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-11-09 20:13:22 -0600

    Added colour switches for buttons

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,21 @@ document.querySelector('button').addEventListener("click", (event) => {
     const clickedButton = event.target;
     document.querySelector("#part1").setAttribute('hidden', 'true');
     const buttonContent = clickedButton.innerHTML;
-    if (buttonContent == 'happy') {
-        document.querySelector("body").style.backgroundColor = 'blue';
-    }
-});
+    switch (buttonContent) {
+        case "Happy":
+            document.querySelector("body").style.backgroundColor = "rgb(255, 253, 55)";
+          break;
+        case "Sad":
+            document.querySelector("body").style.backgroundColor = "rgb(54,69,79)";
+          break;
+        case "Annoyed":
+            document.querySelector("body").style.backgroundColor = "rgb(40, 240, 18)";
+          break;
+        case "Excited":
+            document.querySelector("body").style.backgroundColor = "rgb(246, 38, 129)";
+        case "Angry":
+            document.querySelector("body").style.backgroundColor = "rgb(208, 28, 31)";
+            break;
+        case "Calm":
+            document.querySelector("body").style.backgroundColor = "rgb(147, 202, 237)";
+}})

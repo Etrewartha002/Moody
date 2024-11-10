9151dc688a9d07afb7f5fd342625988557d70f41
ETrewartha <136138918+Etrewartha002@users.noreply.github.com>
2024-11-09 21:51:49 -0600

    Finished up js

--- public/js/index.js
+++ public/js/index.js
@@ -8,26 +8,32 @@ document.querySelectorAll('button').forEach(button => {
             case "Happy":
                 document.querySelector("body").style.backgroundColor = "rgb(255, 253, 55)";
                 document.getElementById("p2").innerText = buttonContent;
+                document.querySelector("#part2").setAttribute('hidden', "false")
                 break;
             case "Sad":
                 document.querySelector("body").style.backgroundColor = "rgb(54,69,79)";
                 document.getElementById("p2").innerText = buttonContent;
+                document.querySelector("#part2").setAttribute('hidden', "false");
                 break;
             case "Annoyed":
                 document.querySelector("body").style.backgroundColor = "rgb(40, 240, 18)";
                 document.getElementById("p2").innerText = buttonContent;
+                document.querySelector("#part2").setAttribute('hidden', "false");
                 break;
             case "Excited":
                 document.querySelector("body").style.backgroundColor = "rgb(246, 38, 129)";
                 document.getElementById("p2").innerText = buttonContent;
+                document.querySelector("#part2").setAttribute('hidden', "false");
                 break;
             case "Angry":
                 document.querySelector("body").style.backgroundColor = "rgb(208, 28, 31)";
                 document.getElementById("p2").innerText = buttonContent;
+                document.querySelector("#part2").setAttribute('hidden', "false");
                 break;
             case "Calm":
                 document.querySelector("body").style.backgroundColor = "rgb(147, 202, 237)";
                 document.getElementById("p2").innerText = buttonContent;
+                document.querySelector("#part2").setAttribute('hidden', "false");
                 break;
             default:
                 console.log("No matching case for button content:", buttonContent);
